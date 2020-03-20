6bb3336293a54ff0a157072bba4fa57e0e66b07e
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-03-19 20:55:54 -0700

    Added react-bootstrap and made a popover for info

--- src/components/PhotoApiCall.js
+++ src/components/PhotoApiCall.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import PhotoDisplay from "./PhotoDisplay";
 export default function PhotoApiCall() {
   const [photo, setPhoto] = useState();
+  const [info, setInfo] = useState();
   useEffect(() => {
     axios
       .get("https://api.unsplash.com/photos/random", {
@@ -14,10 +15,11 @@ export default function PhotoApiCall() {
       .then(response => {
         console.log(response);
         setPhoto(response.data.urls.full);
+        setInfo(response.data)
       })
       .catch(error => {
         console.log(error, "response not found");
       });
   }, []);
-  return <PhotoDisplay photo={photo} />;
+  return <PhotoDisplay photo={photo} info={info} />;
 }
--- src/components/PhotoDisplay.js
+++ src/components/PhotoDisplay.js
@@ -1,21 +1,51 @@
 import React from "react";
 import styled from "styled-components";
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
+import {Image,Container,Popover, Button, OverlayTrigger} from 'react-bootstrap'
 
 const BackGround = styled.div`
-  background-img: url("./sw-bg.jpg");
-`;
-// adding a photo to back ground
+
+`
+
+// adding a photo to background
 const Photo = styled.img`
   max-width: 90%;
   max-height: 90%;
-`;
+`
+const Name = styled.h1`
+
+`
 
 const PhotoDisplay = props => {
+  console.log('photo takers name',props.info)
+  
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">{props.info?.user?.name}</Popover.Title>
+      <Popover.Content>
+        <div>
+        <h2>Total of {props.info?.user?.total_photos} photos</h2>
+        <h2>find them {props.info?.user?.portfolio_url} </h2>
+          </div>
+        
+      </Popover.Content>
+    </Popover>
+  );
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button variant="outline-info"><i class="fas fa-info-circle fa-2x"></i></Button>
+    </OverlayTrigger>
+  );
+  
+ 
   return(
-  <BackGround>
-    <Photo src={props.photo} />;
-  </BackGround>
+  <Container className='container-div'>
+    <Image src={props.photo} fluid/>
+    <Name>
+      {props.info?.description}
+    </Name>
+    <Example/>
+  </Container>
   )
 };
 
