ab0e8b83004d50b5994ed2ef0769bad2e5007e55
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-11-18 17:55:23 -0800

    adding Routes and making Landing page Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import PhotoApiCall from "./components/PhotoApiCall";
 import Header from "./components/Header"
 import "./App.css";
+import LandingPage from "./components/LandingPage";
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <PhotoApiCall />
+      <Header/> 
+      <Route 
+      path="/"
+      exact
+      render={props => <PhotoApiCall {...props}/>}
+      />
+      <LandingPage />
+      
+      
+      
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Link,
+
+} from "React-router-dom";
+
+
+export default function LandingPage(){
+    return(
+   <>
+    <div className="LandingPageMain">
+        <h1>Hello
+            
+        </h1>
+    </div>
+
+   </>
+
+
+
+
+    )
+
+}
