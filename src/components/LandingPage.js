4ede29c641d85fb7fcdbb20cdb9d7a48506fa62a
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-11-24 20:34:07 -0800

    ...

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,13 +5,13 @@ import {
 
 } from "React-router-dom";
 
-
+// add landing page CTA and get a slid into mane page working
 export default function LandingPage(){
     return(
    <>
     <div className="LandingPageMain">
         <h1>Hello
-            
+
         </h1>
     </div>
 
