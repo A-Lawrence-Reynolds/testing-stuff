11f1a1d4b7de95f77dd64c4b6eb54511fa7d9c38
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-12-02 17:49:12 -0800

    ...

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <LandingPage /> 
+      <LandingPage
+      path='/'
+      /> 
       <Route 
-      path="/"
+      path="Photo"
       exact
       render={props => <PhotoApiCall {...props}/>}
       />
