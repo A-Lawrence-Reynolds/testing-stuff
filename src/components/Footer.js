1f17897fd21ce6400f5c7b89f79adf01d61f785a
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2021-02-06 16:27:28 -0800

    ...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ return (
     <>
     <div className='Footer'>
         <p>Footer</p>
-        
+        {/* add in links to other ages and to creator page  */}
 
         <div className='Links'></div>
     </div>
