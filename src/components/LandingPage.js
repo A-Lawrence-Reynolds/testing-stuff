6dc4880bb4da12e7cb1ea5d1eb94a36a30490a80
Adam Reynolds <adam@Adams-Air.home>
2022-03-21 12:13:53 -0700

    added one line of code for footer..

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,22 +6,20 @@ import React from "react";
 // } from "React-router-dom";
 
 // add landing page CTA and get a slid into mane page working
-export default function LandingPage(){
-    return(
-   <>
-    <div className="LandingPageMain">
-       {/* Landing page content   */}
+export default function LandingPage() {
+  return (
+    <>
+      <div className="LandingPageMain">
+        {/* Landing page content   */}
         <p>this is a paragraph tag.</p>
         <h2>Welcome to rand Pic's </h2>
         <h3>find a random picture</h3>
-        <div className="LandingPageSecondary" > 
-             
-        </div>
-    </div>
-    {/* add about the page in footer or build another page */}
-   </>
-
-
-    )
-
+        <div className="LandingPageSecondary"></div>
+      </div>
+      <div>
+        <p>Made by the firey beard developer.</p>
+      </div>
+      {/* add about the page in footer or build another page */}
+    </>
+  );
 }
